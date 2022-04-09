a9a5304b668084efd119f5ee2aadad0dd2b59ff9
francisco-solis99 <franciscosoliscss@gmail.com>
2022-04-09 15:30:59 -0500

    [Delete] initial  instructions to probe in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,3 @@ import { Model } from "./modules/Model.mjs";
 // app will be instance of teh controller
 const app = new Controller(new Model(), new View());
 console.log(app);
-
-app.model.addTodo("Take a nap");
-
-console.log(app.model.todos);
