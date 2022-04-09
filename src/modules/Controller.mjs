f5a6f9c50cbb3a6569845c082c9d9a658ff8ecc5
francisco-solis99 <franciscosoliscss@gmail.com>
2022-04-09 15:37:38 -0500

    [Add] the binding with the todolist when changes to the model and [Add] binding handlers of the vie's events

--- src/modules/Controller.mjs
+++ src/modules/Controller.mjs
@@ -3,5 +3,33 @@ export class Controller {
     // takes in the model and view
     this.model = model;
     this.view = view;
+
+    // binding the todoList when changes to the model
+    this.model.bindTodoListChanged(this.onTodoListChanged);
+
+    // binding the handlers of the events to the view
+    this.view.bindAddTodo(this.handleAddTodo);
+    this.view.bindDeleteTodo(this.handleDeleteTodo);
+    this.view.bindToggleTodo(this.handleToggleTodo);
+
+    // We can also call it once in the constructor to display the initial todos if there are any.
+    this.onTodoListChanged(this.model.todos);
   }
+
+  // method that calls displayTodos every time a todo changes
+  onTodoListChanged = (todos) => this.view.renderTodos(todos);
+
+  // handlers for the evets after the are fired.
+  // The view must listen for those events because they're user input of the view, but it will dispatch the responsibility of what will happen in response to the event to the controller.
+  handleAddTodo = (todoText) => {
+    this.model.addTodo(todoText);
+  };
+
+  handleEditTodo = (id, todoText) => {
+    this.model.editTodo(id, todoText);
+  };
+
+  handleDeleteTodo = (id) => this.model.deleteTodo(id);
+
+  handleToggleTodo = (id) => this.model.toggleTodo(id);
 }
